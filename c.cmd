24e7043881bdae601a871fc5d844cc6a1c0e57f2
Kirk Shoop <kirk.shoop@microsoft.com>
2013-03-29 09:13:08 -0700

    add example build script

---
+++ c.cmd
@@ -0,0 +1,5 @@
+@echo off
+pushd C:\Users\kshoop\Desktop\source\modernscratch
+call \\vriska\shared\stlbuild.bat wcfb01-2013.03.22
+cl -EHsc -IC:\Users\kshoop\Desktop\source\Rx\Rx\CPP\src -IC:\Users\kshoop\Desktop\source\Rx\Ix\CPP\src -IC:\Users\kshoop\Desktop\source\libraries -DUNICODE -D_UNICODE -DDELAY_ON_WORKER_THREAD -ZI -Fd"testbench.pdb" modernscratch.cpp
+popd
